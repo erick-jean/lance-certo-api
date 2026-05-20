1fc787bbe618d8babdf29f6760a3e834fb20bcea
Erick Jean <erickjean36@gmail.com>
2026-05-20 10:52:01 -0400

    feat: add Mercado Pago module to subscription imports

--- src/modules/subscription/subscription.module.ts
+++ src/modules/subscription/subscription.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { MercadoPagoModule } from '../mercado-pago/mercado-pago.module';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MercadoPagoModule],
   controllers: [SubscriptionController],
   providers: [SubscriptionService],
 })
