d88ff92380fa7fc8047c853db00723c2b6aa9749
Erick Jean <erickjean36@gmail.com>
2026-05-25 15:58:20 -0400

    fix: importa auth module no modulo fipe

--- src/modules/fipe/fipe.module.ts
+++ src/modules/fipe/fipe.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FipeService } from './fipe.service';
 import { FipeController } from './fipe.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [FipeController],
   providers: [FipeService],
 })
